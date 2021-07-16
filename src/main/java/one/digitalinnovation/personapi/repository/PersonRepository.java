e50685804a10ba413474ba6879144e2c42f571f4
Catarina Ramalho <catarinaramalhoh@gmail.com>
2021-07-15 21:31:26 -0300

    Finalization

--- src/main/java/one/digitalinnovation/personapi/repository/PersonRepository.java
+++ src/main/java/one/digitalinnovation/personapi/repository/PersonRepository.java
@@ -1,7 +1,9 @@
 package one.digitalinnovation.personapi.repository;
 
 import one.digitalinnovation.personapi.entity.Person;
+import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
+
 
 public interface PersonRepository extends JpaRepository<Person, Long> {
 }
